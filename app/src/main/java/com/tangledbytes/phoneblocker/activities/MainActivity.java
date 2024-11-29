79b31547254288fb583bcd9d6421d43d9312b211
raoyongqi <1152416230@qq.com>
2024-11-29 16:17:45 +0800

    1129

--- app/src/main/java/com/tangledbytes/phoneblocker/activities/MainActivity.java
+++ app/src/main/java/com/tangledbytes/phoneblocker/activities/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,22 +28,34 @@ import com.tangledbytes.phoneblocker.utils.BlockerSession;
 import com.tangledbytes.phoneblocker.utils.Constants;
 import com.tangledbytes.phoneblocker.utils.Utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.MissingFormatArgumentException;
+import java.util.TimeZone;
 import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private SwitchCompat switchPreventPowerOff;
-    private SwitchCompat switchBlockNotifications;
-    private SwitchCompat switchBlockCalls;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
     private Button btnLockDevice;
     private TextView tvDurationHours;
+
+    final Calendar cal = Calendar.getInstance();
+
     private TextView tvDurationMinutes;
     private TextView tvDurationSeconds;
     //TODO: set time to hrs = 1, min = 30, seconds = 0 in release version
-    private int durationHours = 0;
-    private int durationMinutes = 0;
-    private int durationSeconds = 10;
+    // 假设你设置了时间格式
+
+    // 日期和时间格式化
+
+
+
+    private int durationHours = 0 ;  // 使用当前时间的小时数
+    private int durationMinutes=0;  // 使用当前时间的分钟数
+    private int durationSeconds = 10;  // 假设秒数初始化为10
 
     private class OnLockStateChanged extends BroadcastReceiver {
         @Override
@@ -69,8 +83,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         new BlockerSession(this).invalidateSession();
         setContentView(R.layout.activity_main);
         setUpToolbar();
+        final SimpleDateFormat duTime = new SimpleDateFormat("HH:mm", Locale.CHINA);
+        TimeZone timeZone = TimeZone.getTimeZone("Asia/Shanghai");
+
+
+
+        duTime.setTimeZone(timeZone);
+        final String dufTime = duTime.format(cal.getTime());  // 获取格式化的时间字符串，如 "07:35"
+
+        // 使用正则分割时间字符串获取小时和分钟
+        String[] timeParts = dufTime.split(":");
+        int hour = Integer.parseInt(timeParts[0]);  // 获取小时
+        int minute = Integer.parseInt(timeParts[1]);  // 获取分钟
+
+        durationHours = 0;
+        durationMinutes = 15;  // 默认持续时间为 15 分钟
+
+        if ((hour >= 22 && hour <= 23) || (hour >= 0 && hour <= 5)) {
+            durationHours = 1;  // 如果是晚上 10 点到早上 5 点之间，持续时间为 1 小时
+            durationMinutes = 0;  // 不需要分钟
+        }
 
         setUpViews();
+        initTimeDisplay();
+
+
         IntentFilter filter = new IntentFilter();
         filter.addAction(Constants.BC_DEVICE_LOCKED);
         filter.addAction(Constants.BC_DEVICE_UNLOCKED);
@@ -101,9 +138,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setUpViews() {
-        switchPreventPowerOff = findViewById(R.id.switch_prevent_power_off);
-        switchBlockNotifications = findViewById(R.id.switch_block_notifications);
-        switchBlockCalls = findViewById(R.id.switch_block_calls);
+
         tvDurationHours = findViewById(R.id.tv_duration_hours);
         tvDurationMinutes = findViewById(R.id.tv_duration_minutes);
         tvDurationSeconds = findViewById(R.id.tv_duration_seconds);
@@ -126,9 +161,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         updateDurationTextViews();
 
         // TODO: Add code for blocking calls and notifications
-        Utils.disable(findViewById(R.id.parent_prevent_phone_calls));
-        Utils.disable(findViewById(R.id.parent_prevent_phone_notifs));
+
     }
+
+    private void initTimeDisplay() {
+        // 假设布局中有一个 id 为 tv_current_time 的 TextView
+        TextView tvCurrentTime = findViewById(R.id.tv_current_time);
+
+        // 定义Runnable，更新每秒钟的时间
+        Runnable updateTimeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // 获取当前时间
+                final Calendar cal = Calendar.getInstance();
+                TimeZone timeZone = TimeZone.getTimeZone("Asia/Shanghai");
+                // 日期和时间格式化
+                final SimpleDateFormat sdfDate = new SimpleDateFormat("EEEE, dd.MM.yyyy", Locale.CHINA);
+                sdfDate.setTimeZone(timeZone);
+
+                final SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm", Locale.CHINA);
+                sdfTime.setTimeZone(timeZone);
+
+                final SimpleDateFormat sdfSeconds = new SimpleDateFormat("ss", Locale.CHINA);
+                sdfSeconds.setTimeZone(timeZone);
+
+                // 获取当前日期、时间和秒数
+                final String currentDate = sdfDate.format(cal.getTime());
+                final String currentTime = sdfTime.format(cal.getTime());
+                final String currentSeconds = sdfSeconds.format(cal.getTime());
+
+
+                // 在UI线程更新TextView
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 显示日期和时间
+                        tvCurrentTime.setText(currentDate + "\n" + currentTime + ":" + currentSeconds);
+                    }
+                });
+
+                // 每隔1秒更新一次
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        // 启动Runnable，开始更新时间
+        handler.post(updateTimeRunnable);
+    }
+
 
     @Override
     public void onClick(View view) {
@@ -174,7 +254,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         durationMillis += durationSeconds * 1000L;
 
         Intent intentBlockerService = new Intent(this, BlockerService.class);
-        session.createSession(durationMillis, switchPreventPowerOff.isChecked(), switchBlockCalls.isChecked(), switchBlockNotifications.isChecked());
+        session.createSession(durationMillis, true, false, false);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
             startForegroundService(intentBlockerService);
         else startService(intentBlockerService);
